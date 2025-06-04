73a32202b675011945e989bbf8af53070600334b
Beatriz Arevalo Freitas <131297673+biaArevaloFreitas@users.noreply.github.com>
2025-06-04 20:43:25 -0300

    fix: corrigir erro na conexão do banco de dados

---
+++ control_estoque_a3.sql
@@ -0,0 +1,54 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Movimentacoes;
+DROP TABLE IF EXISTS Produtos;
+DROP TABLE IF EXISTS Unidade_Medida;
+DROP TABLE IF EXISTS Categorias;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Criação da tabela de categorias
+CREATE TABLE Categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    tamanho VARCHAR(50) NOT NULL,
+    embalagem VARCHAR(50) NOT NULL
+);
+
+-- Criação da tabela de produtos
+CREATE TABLE Produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    quantidade INT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    qtd_minima INT NOT NULL,
+    qtd_maxima INT NOT NULL,
+    categoria_id INT NOT NULL,
+    unidade_medida VARCHAR(10) NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES Categorias(id)
+);
+
+-- Criação da tabela de movimentações
+CREATE TABLE Movimentacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    produto_id INT NOT NULL,
+    tipo ENUM('entrada', 'saida') NOT NULL,
+    quantidade INT NOT NULL,
+    data_movimentacao DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    observacoes TEXT NOT NULL,
+    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+);
+
+SELECT 
+    p.nome AS nome_produto,
+    p.preco,
+    p.quantidade,
+    p.qtd_minima,
+    p.qtd_maxima,
+    c.nome AS categoria
+FROM Produtos p
+JOIN Categorias c ON p.categoria_id = c.id;
+
+SELECT * FROM Produtos;
+SELECT * FROM Categorias;
+SELECT * FROM Movimentacoes;
